770c855fcbe052fb6313f319be77f50224f269e7
matija2208 <matijacetrovic@gmail.com>
2021-03-24 15:56:09 +0100

    Dodat delete i edit u bazu

--- back/index.js
+++ back/index.js
@@ -69,3 +69,54 @@ app.post("/api/posts", async function(req,res){
         });
     }
 });
+app.delete("/api/posts/:id",async function(req,res){
+    try{
+        const postId = req.params.id;
+        const newPost = await post.findById(postId);
+        const deletedPost = await newPost.delete();
+        res.send({
+            uspesnost:true,
+            objava:deletedPost,
+        });
+    }
+    catch(err){
+        res.send({
+            uspesnost:false,
+            poruka:err.message
+        });
+    }
+})
+
+app.post("/api/posts/:id/edit", async function(req,res){
+    try{
+        const postId = req.params.id;
+        const newPost = await post.findById(postId);
+        const postProp = req.params.prop;
+
+        newPost.sport=req.body.sport;
+        newPost.tip=req.body.tip;
+        newPost.sadrzaj=req.body.sadrzaj;
+        newPost.lozinka=req.body.lozinka;
+        
+        newPost.info.mesto=req.body.info.mesto;
+        newPost.info.datum=req.body.info.datum;
+        newPost.info.vreme=req.body.info.vreme;
+        
+        newPost.kontakt.ime=req.body.kontakt.ime;
+        newPost.kontakt.telefon=req.body.kontakt.telefon;
+        newPost.kontakt.mail=req.body.kontakt.mail;
+        const savedPost=await newPost.save();
+        console.log("post edit pogodjen");
+        console.log(req.body);
+        res.send({
+            uspesnost:true,
+            objava:savedPost,
+        });
+    }
+    catch(err){
+        res.send({
+            uspesnost:false,
+            poruka:err.message
+        });
+    }
+});
