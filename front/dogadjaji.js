81d51a8330f07eb7c694352a1adabf406de6957a
zmici <zmilica.567@gmail.com>
2021-03-30 19:04:25 +0200

    dodavanje polja izmena

--- front/dogadjaji.js
+++ front/dogadjaji.js
@@ -211,6 +211,100 @@ function vidi_jos(id)
         <button class="nazad" onClick="vidi_manje()">NAZAD</button> 
         
     </div>`
+
+     ` 
+     <section>
+      <div class="forma" id="formaDiv">
+     <form id="forma">
+       <h1 class="form-title">NOVI DOGAĐAJ</h1><br><br>
+
+       <h2 class="form-1">SPORT :</h2>
+       <select class="vreme" id="sport_select" onchange="other_select()">
+         <option>Fudbal</option>
+         <option>Košarka</option>
+         <option>Rukomet</option>
+         <option>Odbojka</option>
+         <option>Tenis</option>
+         <option>Stoni tenis</option>
+         <option>Gimnastika</option>
+         <option>Plivanje</option>
+         <option>Vaterpolo</option>
+         <option>Šah</option>
+         <option>Workout</option>
+         <option>Drugo</option>
+       </select>
+       <!-- INPUT SPORTA -->
+       <input class="add-input" id="sport_input" type="hidden" placeholder="Ime sporta koji će se održati:" />
+       <p class="valid-error hidden" id="sport_error">Ime sporta moze sadrzati samo slova.</p>
+
+
+       <!--INPUT ZA BIRANJE TIPOVA RADIO-->
+       <h2 class="form-1">VRSTA DOGAĐAJA:</h2>
+     
+      <div class="tip-div"> 
+       <p class="tip1">UTAKMICA</p>
+       <input type="radio" name="gender" class="tip" value="utakmica"><br><br>
+      </div>
+
+      <br>
+      <div class="tip-div">
+       <p class="tip1">TURNIR</p>
+       <input type="radio" name="gender" class="tip" value="turnir"><br><br>
+      </div>
+
+      <br>
+      <div class="tip-div">
+       <p class="tip1">PRIJATELJSKI MEČ</p>
+       <input type="radio" name="gender" class="tip" value="prijateljski meč"><br><br>
+      </div>
+
+   <h2 class="form-1">PODACI:</h2>
+
+   <!--INPUT ZA DATUM-->
+   <h3 class="form-categ">Datum:</h3>
+   <input type="date" class="vreme" id="datum_input" />
+
+   <!--INPUT ZA VREME-->
+   <h3 class="form-categ">Vreme:</h3>
+   <input type="time" class="vreme" id="vreme_input" />
+
+   <!--INPUT ZA LOKACIJU-->
+   <h3 class="form-categ">Mesto:</h3>
+   <input class="add-input" id="lokacija_input" type="text" placeholder="Mesto gde će se održati događaj:" />
+
+   <!--INPUT ZA SADRZAJ-->
+   <h3 class="form-categ">Dodatne informacije:</h3>
+      <div class="sadrzaj" id="sadrzaj_input" contenteditable></div>
+       
+       <h2 class="form-1">KONTAKT:</h2>
+
+       <h3 class="form-categ">Ime i prezime:</h3>
+
+       <!--INPUT ZA IME-->
+       <input class="add-input" id="ime_input" type="text" placeholder="Vase ime i prezime:" />
+       <p class="valid-error hidden" id="ime_error">Ime sme sadrzati samo slova.</p>
+
+       <!--INPUT ZA TELEFON-->
+       <h3 class="form-categ">Broj telefona:</h3>
+       <input class="add-input" id="tel_input" type="tel" placeholder="Broj telefona organizatora:" />
+       <p class="valid-error hidden" id="tel_error">Broj telefona mora biti spojeni niz koji sadrzi samo brojeve (npr. 0631234567).</p>
+
+       <!--INPUT ZA MAIL-->
+       <h3 class="form-categ">E-Mail adresa:</h3>
+       <input class="add-input" id="mail_input" type="email" placeholder="Adresa E-Pošte organizatora:" />
+       <p class="valid-error hidden" id="mail_error">E-Mail adresa mora sadrzati validnu adresu (example@[mail].com)</p>
+
+       <!--INPUT ZA LOZINKU-->
+       <h3 class="form-categ">Lozinka:</h3>
+       <input class="add-input" id="pass_input" type="password" placeholder="Lozinka objave, lozinka mora sadržati više od 6 slova, barem jedan broj i barem jedno slovo:" />
+       <p class="valid-error hidden" id="pass_error">Lozinka mora sadrzati vise od 6 slova i mora sadrzati barem jedan broj i jedno slovo.</p>
+
+     
+       <input type="button" class="add-button" id="dodajobjavu" value="DODAJ OBJAVU" onclick="validiraj()" ></input>
+     </form>
+   </div>
+</section>
+     `
     vidijos.innerHTML=tekst;
 }
 
